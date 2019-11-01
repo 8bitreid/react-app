5368cbde282e5b4896a077f689df1ab33f3363eb
Reid Mewborne <8bitreid@users.noreply.github.com>
2019-10-31 22:02:57 -0500

    Update hello Component (#5)
    *  adding missing files
    
    * adding description

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Hello } from "./components/Hello";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Learning React</h1>
+      <Hello compiler="TypeScript" framework="React" />
     </div>
   );
 }
